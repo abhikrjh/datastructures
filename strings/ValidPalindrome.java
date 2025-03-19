f803a1d66d492a63b787c2ba260f75a40c473896
abhikrjh <abhi.kr.jh@gmail.com>
2025-03-19 07:17:58 +0530

    Updated Palindrome

--- strings/ValidPalindrome.java
+++ strings/ValidPalindrome.java
@@ -6,11 +6,35 @@ public class ValidPalindrome {
 //    removing all non-alphanumeric characters, it reads the same forward and backward.
 //    Alphanumeric characters include letters and numbers.
 
-    public boolean checkPalindrome(String input) {
-        if (input.length() == 0)
+    private boolean isValidPalindrome(String str) {
+
+        if (null == str || str.isEmpty())
             return false;
+
+        str = str.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+
+        System.out.println("Removed all non-alphanumeric characters : " + str);
+
+        char[] chars = str.toCharArray();
+
+        for (int i = 0; i < chars.length / 2; i++) {
+
+            if (chars[i] != chars[chars.length - (i + 1)]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkPalindrome(String input) {
+
+        if (null == input || input.isEmpty())
+            return false;
+
         String[] trimmedInput = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().split("");
+
         int len = trimmedInput.length;
+
         for (int i = 0; i < len / 2; i++) {
             if (!(trimmedInput[i].equalsIgnoreCase(trimmedInput[len - (i + 1)]))) {
                 return false;
@@ -20,10 +44,8 @@ public class ValidPalindrome {
     }
 
     public static void main(String[] args) {
-        ValidPalindrome v= new ValidPalindrome();
+        ValidPalindrome v = new ValidPalindrome();
         String input = "A man, a plan, a canal: Panama";
-        System.out.println(v.checkPalindrome(input));
+        System.out.println(v.isValidPalindrome(input));
     }
-
-
 }
