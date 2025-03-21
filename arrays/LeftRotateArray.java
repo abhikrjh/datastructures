7bcb177a154f363d789252dc46aa00e1e07c3ad5
abhikrjh <abhi.kr.jh@gmail.com>
2025-03-21 07:13:02 +0530

    Left rotate the array by k element

--- arrays/LeftRotateArray.java
+++ arrays/LeftRotateArray.java
@@ -26,10 +26,47 @@ public class LeftRotateArray {
         return arr;
     }
 
+    private static int[] leftRotate(int[] arr, int k){
+
+        // reverse the entire array
+
+        reverseArr(arr, 0, arr.length - 1);
+
+        // reverse the right most k element
+
+        reverseArr(arr, arr.length - k, arr.length - 1);
+
+        // reverse the remaining left element
+
+        return reverseArr(arr, 0, arr.length - (k + 1));
+    }
+
+
+    private static int[] reverseArr(int[] arr, int start, int end){
+
+        while(start < end){
+
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+
+        return arr;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = new int[]{1, 2, 3, 4, 5};
+        int k = 3;
 
-        System.out.println(Arrays.toString(leftRotateArrayByOnePlace(arr)));
+//        System.out.println(Arrays.toString(leftRotateArrayByOnePlace(arr)));
+
+        // 5, 4, 3, 2, 1 - 1st step (reversed the entire array)
+        // 5, 4, 1, 2, 3 - 2nd step (reversed the right most k(3) element (3, 2, 1 became 1, 2, 3))
+        // 4, 5, 1, 2, 3 - 3rd step (reversed the remaining left element) (5, 4 became 4, 5)
+
+        System.out.println( Arrays.toString(leftRotate(arr, k)));
     }
 }
