36cef9454940e97c5a6dc6c1b3a81213cbe385c0
abhikrjh <abhi.kr.jh@gmail.com>
2026-03-18 07:13:36 +0530

    Leetcode #53 : Maximum subarray

--- arrays/MaximumSubArray.java
+++ arrays/MaximumSubArray.java
@@ -28,10 +28,31 @@ public class MaximumSubArray {
         return largestSum;
     }
 
+    public static int maxSubArray(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        for(int i=0; i< nums.length; i++) {
+
+            currentSum += nums[i];
+
+            maxSum = Math.max(currentSum, maxSum);
+
+            if(currentSum < 0) {
+                currentSum = 0;
+            }
+
+        }
+        return maxSum;
+    }
+
     public static void main(String[] args) {
         MaximumSubArray m = new MaximumSubArray();
         int[] array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        System.out.println(m.largestContiguousSubArraySum(array));
+//        System.out.println(m.largestContiguousSubArraySum(array));
+
+        System.out.println(m.maxSubArray(array));
     }
 
 
