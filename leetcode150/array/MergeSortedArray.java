15baf8e5973b6d1328dcd9c0219906368ee241c7
Abhinav <abhi.kr.jh@gmail.com>
2026-04-21 07:52:36 +0530

    Optimized merge sorted array code

--- leetcode150/array/MergeSortedArray.java
+++ leetcode150/array/MergeSortedArray.java
@@ -35,7 +35,25 @@ import java.util.Arrays;
 
 public class MergeSortedArray {
 
-    private static void merge(int[] nums1, int m, int[] nums2, int n) {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(j >= 0) {
+
+            if(i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            }else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+    private static void merge1(int[] nums1, int m, int[] nums2, int n) {
 
         int p1 = m - 1;
         int p2 = n - 1;
